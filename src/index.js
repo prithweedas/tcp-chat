d26d8507df6cde355fdf5391cface4d4a57a1528
Prithwee Das <prithweedas123@gmail.com>
2018-04-30 00:57:20 +0530

    new flags --host & --connect

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import { fork } from 'child_process'
 const [serverOrClient, ...otherArgs] = process.argv.slice(2)
 
 switch (serverOrClient) {
-case '--server':
+case '--host':
 	fork('./src/server', [...otherArgs])
 	break
 
-case '--client':
+case '--connect':
 	fork('./src/client', [...otherArgs])
 	break
+
 default:
 	console.log(`Invalid Arguments!
 Arguments must be one of the followings:
-	--server <PORT>
-	--client <HOST> <PORT>`)
+	--host <PORT || 4000>
+	--connect <HOST || 127.0.0.1> <PORT || 4000>`)
 	break
 }
